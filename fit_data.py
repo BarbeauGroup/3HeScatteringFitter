d22eb290ca8e318428aed1bb3c0652753a1ecbc9
Sam <sch52@phy.duke.edu>
2020-07-20 11:59:57 -0400

    Some small updates

--- fit_data.py
+++ fit_data.py
@@ -20,7 +20,10 @@ for line in inpFile:
   graph.SetPoint(pointNum,E,contents)
   pointNum+=1
 
-nBins=300
+if not "208" in sys.argv[1]:
+  nBins=600
+else:
+  nBins=300
 hist=ROOT.TH1D("hist",";Energy (MeV);Counts",nBins,0,30)
 for i in range(0,nBins):
   val=graph.Eval(hist.GetBinCenter(i+1))
@@ -92,11 +95,11 @@ if "208" in sys.argv[1]:
   
   if not "Krasz" in sys.argv[1]:
     iasMean=ROOT.RooRealVar("iasMean","IAS",16.2,16.0,16.5)
-    iasAmp=ROOT.RooRealVar("iasAmp","iasAmp",500,50,800)
+    iasAmp=ROOT.RooRealVar("iasAmp","iasAmp",700,50,900)
   else:
     iasMean=ROOT.RooRealVar("iasMean","IAS",15.0,13.8,15.0)
     iasAmp=ROOT.RooRealVar("iasAmp","iasAmp",30,30,400)
-  iasSigma=ROOT.RooRealVar("iasSigma","iasSigma",0.18,0.15,0.20)
+  iasSigma=ROOT.RooRealVar("iasSigma","iasSigma",0.16,0.14,0.20)
   iasGauss=ROOT.RooGaussian("iasGauss","iasGauss",eVar,iasMean,iasSigma)
 
   gtrMean=ROOT.RooRealVar("gtrMean","GTR",15,14,18)
@@ -110,22 +113,22 @@ if "208" in sys.argv[1]:
   
   sdrMean=ROOT.RooRealVar("sdrMean","SDR",22,20,29)
   sdrSigma=ROOT.RooRealVar("sdrSigma","sdrSigma",3,1.4,8)
-  sdrAmp=ROOT.RooRealVar("sdrAmp","sdrAmp",100,100,2000)
+  sdrAmp=ROOT.RooRealVar("sdrAmp","sdrAmp",100,100,3000)
   sdrGauss=ROOT.RooGaussian("sdrGauss","sdrGauss",eVar,sdrMean,sdrSigma)
   
-  gtr1Mean=ROOT.RooRealVar("gtr1Mean","GTR1",8,7.5,8.5)
+  gtr1Mean=ROOT.RooRealVar("gtr1Mean","GTR1",8,5.0,8.5)
   gtr1Sigma=ROOT.RooRealVar("gtr1Sigma","gtr1Sigma",1.0,.3,5.5)
   gtr1Amp=ROOT.RooRealVar("gtr1Amp","gtr1Amp",20,10,2000)
   gtr1Gauss=ROOT.RooGaussian("gtr1Gauss","gtr1Gauss",eVar,gtr1Mean,gtr1Sigma)
   
   gtr2Mean=ROOT.RooRealVar("gtr2Mean","GTR2",9.5,9.0,10)
   gtr2Sigma=ROOT.RooRealVar("gtr2Sigma","gtr2Sigma",1.0,.5,3.0)
-  gtr2Amp=ROOT.RooRealVar("gtr2Amp","gtr2Amp",20,10,2000)
+  gtr2Amp=ROOT.RooRealVar("gtr2Amp","gtr2Amp",20,5,2000)
   gtr2Gauss=ROOT.RooGaussian("gtr2Gauss","gtr2Gauss",eVar,gtr2Mean,gtr2Sigma)
   
   if not "Krasz" in sys.argv[1]:
-    n12Mean=ROOT.RooRealVar("n12Mean","12N (bgnd)",15.5,15.4,15.7)
-    n12Sigma=ROOT.RooRealVar("n12Sigma","n12Sigma",0.18,0.10,0.19)
+    n12Mean=ROOT.RooRealVar("n12Mean","12N (bgnd)",15.5,15.4,15.6)
+    n12Sigma=ROOT.RooRealVar("n12Sigma","n12Sigma",0.05,0.05,0.10 )
     n12Amp=ROOT.RooRealVar("n12Amp","n12Amp",20,20,2000)
     n12Gauss=ROOT.RooGaussian("n12Gauss","n12Gauss",eVar,n12Mean,n12Sigma)
   
@@ -164,7 +167,7 @@ if "208" in sys.argv[1]:
   
   g7Mean=ROOT.RooRealVar("g7Mean","g7Mean",5.5,5.0,7.0)
   g7Sigma=ROOT.RooRealVar("g7Sigma","g7Sigma",0.28,0.20,0.31)
-  g7Amp=ROOT.RooRealVar("g7Amp","g7Amp",10,1,500)
+  g7Amp=ROOT.RooRealVar("g7Amp","g7Amp",20,10,500)
   g7Gauss=ROOT.RooGaussian("g7Gauss","g7Gauss",eVar,g7Mean,g7Sigma)
   
   if not "Krasz" in sys.argv[1]:
@@ -183,66 +186,66 @@ if "208" in sys.argv[1]:
   
   model=ROOT.RooAddPdf("model","model",pdfList,ampList)
 else:
-  qfcAmp=ROOT.RooRealVar("qfcAmp","qfcAmp",40000,35000,80000)
+  qfcAmp=ROOT.RooRealVar("qfcAmp","qfcAmp",500000,450000,600000)
   
   iasMean=ROOT.RooRealVar("iasMean","IAS",17.5,17,18)
   iasSigma=ROOT.RooRealVar("iasSigma","iasSigma",0.18,0.15,0.20)
-  iasAmp=ROOT.RooRealVar("iasAmp","iasAmp",1500,1000,2000)
+  iasAmp=ROOT.RooRealVar("iasAmp","iasAmp",100000,10000,250000)
   iasGauss=ROOT.RooGaussian("iasGauss","iasGauss",eVar,iasMean,iasSigma)
 
   gtrMean=ROOT.RooRealVar("gtrMean","GTR",17,16.5,18.5)
-  gtrSigma=ROOT.RooRealVar("gtrSigma","gtrSigma",2.0,2.0,3)
-  gtrAmp=ROOT.RooRealVar("gtrAmp","gtrAmp",35000,30000,50000)
+  gtrSigma=ROOT.RooRealVar("gtrSigma","gtrSigma",1.0,1.0,2)
+  gtrAmp=ROOT.RooRealVar("gtrAmp","gtrAmp",35000,30000,200000)
   gtrGauss=ROOT.RooGaussian("gtrGauss","gtrGauss",eVar,gtrMean,gtrSigma)
   
   gtr1Mean=ROOT.RooRealVar("gtr1Mean","GTR1",6,3,8.5)
-  gtr1Sigma=ROOT.RooRealVar("gtr1Sigma","gtr1Sigma",1.0,1.0,5.5)
-  gtr1Amp=ROOT.RooRealVar("gtr1Amp","gtr1Amp",5000,4000,10000)
+  gtr1Sigma=ROOT.RooRealVar("gtr1Sigma","gtr1Sigma",1.0,1.0,4)
+  gtr1Amp=ROOT.RooRealVar("gtr1Amp","gtr1Amp",5000,4000,70000)
   gtr1Gauss=ROOT.RooGaussian("gtr1Gauss","gtr1Gauss",eVar,gtr1Mean,gtr1Sigma)
-  
+  '''
   gtr2Mean=ROOT.RooRealVar("gtr2Mean","GTR2",9.5,9.0,10)
-  gtr2Sigma=ROOT.RooRealVar("gtr2Sigma","gtr2Sigma",2.0,1.5,4.0)
-  gtr2Amp=ROOT.RooRealVar("gtr2Amp","gtr2Amp",3000,2000,9000)
+  gtr2Sigma=ROOT.RooRealVar("gtr2Sigma","gtr2Sigma",2.0,1.5,5.0)
+  gtr2Amp=ROOT.RooRealVar("gtr2Amp","gtr2Amp",3000,2000,50000)
   gtr2Gauss=ROOT.RooGaussian("gtr2Gauss","gtr2Gauss",eVar,gtr2Mean,gtr2Sigma)
-  
-  sdrMean=ROOT.RooRealVar("sdrMean","SDR",24,24,25)
-  sdrSigma=ROOT.RooRealVar("sdrSigma","sdrSigma",3.5,3.0,4)
-  sdrAmp=ROOT.RooRealVar("sdrAmp","sdrAmp",50000,50000,75000)
+  '''
+  sdrMean=ROOT.RooRealVar("sdrMean","SDR",24,23,25)
+  sdrSigma=ROOT.RooRealVar("sdrSigma","sdrSigma",3.5,2.0,5)
+  sdrAmp=ROOT.RooRealVar("sdrAmp","sdrAmp",250000,150000,350000)
   sdrGauss=ROOT.RooGaussian("sdrGauss","sdrGauss",eVar,sdrMean,sdrSigma)
   
   ############
   #Background#
   ############
-  n12Mean=ROOT.RooRealVar("n12Mean","12N (bgnd)",15.5,15.4,15.7)
-  n12Sigma=ROOT.RooRealVar("n12Sigma","n12Sigma",0.18,0.10,0.19)
-  n12Amp=ROOT.RooRealVar("n12Amp","n12Amp",20,20,1000)
+  n12Mean=ROOT.RooRealVar("n12Mean","12N (bgnd)",15.5,15.4,15.95)
+  n12Sigma=ROOT.RooRealVar("n12Sigma","n12Sigma",0.18,0.02,0.19)
+  n12Amp=ROOT.RooRealVar("n12Amp","n12Amp",20,20,2000)
   n12Gauss=ROOT.RooGaussian("n12Gauss","n12Gauss",eVar,n12Mean,n12Sigma)
   
-  n12Mean2=ROOT.RooRealVar("n12Mean2","12N (bgnd)",16.75,16.4,17.)
-  n12Sigma2=ROOT.RooRealVar("n12Sigma2","n12Sigma2",0.18,0.10,0.19)
-  n12Amp2=ROOT.RooRealVar("n12Amp2","n12Amp2",20,20,1000)
+  n12Mean2=ROOT.RooRealVar("n12Mean2","12N (bgnd)",16.8,16.6,17.)
+  n12Sigma2=ROOT.RooRealVar("n12Sigma2","n12Sigma2",0.10,0.01,0.15)
+  n12Amp2=ROOT.RooRealVar("n12Amp2","n12Amp2",1000,30,2000)
   n12Gauss2=ROOT.RooGaussian("n12Gauss2","n12Gauss2",eVar,n12Mean2,n12Sigma2)
   
   f16Mean=ROOT.RooRealVar("f16Mean","16F (bgnd)",20.6,20,21)
-  f16Sigma=ROOT.RooRealVar("f16Sigma","f16Sigma",0.18,0.10,0.19)
-  f16Amp=ROOT.RooRealVar("f16Amp","f16Amp",20,20,1000)
+  f16Sigma=ROOT.RooRealVar("f16Sigma","f16Sigma",0.18,0.05,0.19)
+  f16Amp=ROOT.RooRealVar("f16Amp","f16Amp",20,20,8000)
   f16Gauss=ROOT.RooGaussian("f16Gauss","f16Gauss",eVar,f16Mean,f16Sigma)
   
   f16Mean2=ROOT.RooRealVar("f16Mean2","16F (bgnd)",18.75,18.4,19.)
   f16Sigma2=ROOT.RooRealVar("f16Sigma2","f16Sigma2",0.18,0.10,0.19)
-  f16Amp2=ROOT.RooRealVar("f16Amp2","f16Amp2",20,20,1000)
+  f16Amp2=ROOT.RooRealVar("f16Amp2","f16Amp2",20,20,8000)
   f16Gauss2=ROOT.RooGaussian("f16Gauss2","f16Gauss2",eVar,f16Mean2,f16Sigma2)
   
   f16Mean3=ROOT.RooRealVar("f16Mean3","16F (bgnd)",14.0,13.5,14.5)
-  f16Sigma3=ROOT.RooRealVar("f16Sigma3","f16Sigma3",0.18,0.10,0.19)
-  f16Amp3=ROOT.RooRealVar("f16Amp3","f16Amp3",20,20,1000)
+  f16Sigma3=ROOT.RooRealVar("f16Sigma3","f16Sigma3",0.18,0.05,0.19)
+  f16Amp3=ROOT.RooRealVar("f16Amp3","f16Amp3",20,20,8000)
   f16Gauss3=ROOT.RooGaussian("f16Gauss3","f16Gauss3",eVar,f16Mean3,f16Sigma3)
   
-  pdfList=ROOT.RooArgList(qfc,iasGauss,gtrGauss,gtr1Gauss,gtr2Gauss,sdrGauss)
+  pdfList=ROOT.RooArgList(qfc,iasGauss,gtrGauss,gtr1Gauss,sdrGauss)
   backgroundPdfList=ROOT.RooArgList(n12Gauss,n12Gauss2,f16Gauss,f16Gauss2,f16Gauss3)
   pdfList.add(backgroundPdfList)
   
-  ampList=ROOT.RooArgList(qfcAmp,iasAmp,gtrAmp,gtr1Amp,gtr2Amp,sdrAmp)
+  ampList=ROOT.RooArgList(qfcAmp,iasAmp,gtrAmp,gtr1Amp,sdrAmp)
   backgroundAmpList=ROOT.RooArgList(n12Amp,n12Amp2,f16Amp,f16Amp2,f16Amp3)
   ampList.add(backgroundAmpList)
   model=ROOT.RooAddPdf("model","model",pdfList,ampList)
@@ -251,14 +254,14 @@ res = model.fitTo(dataHist,ROOT.RooFit.Save(1),ROOT.RooFit.Extended(1))
 
 c2=ROOT.TCanvas("c2","c2")
 frame=eVar.frame()
-dataHist.plotOn(frame,ROOT.RooFit.LineColor(ROOT.kBlack),ROOT.RooFit.Binning(100))
+dataHist.plotOn(frame,ROOT.RooFit.LineColor(ROOT.kBlack),ROOT.RooFit.Binning(300))
 model.plotOn(frame)
 model.plotOn(frame,ROOT.RooFit.Components("qfc"),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kGray))
 model.plotOn(frame,ROOT.RooFit.Components("iasGauss"),ROOT.RooFit.LineColor(ROOT.kRed))
 model.plotOn(frame,ROOT.RooFit.Components("gtr1Gauss"),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kViolet))
 model.plotOn(frame,ROOT.RooFit.Components("sdrGauss"),ROOT.RooFit.LineStyle(ROOT.kDashed),ROOT.RooFit.LineColor(ROOT.kRed))
 model.plotOn(frame,ROOT.RooFit.Components("gtrGauss"),ROOT.RooFit.LineStyle(ROOT.kSolid),ROOT.RooFit.LineColor(ROOT.kMagenta))
-model.plotOn(frame,ROOT.RooFit.Components("gtr2Gauss"),ROOT.RooFit.LineStyle(ROOT.kDotted),ROOT.RooFit.LineColor(ROOT.kViolet))
+#model.plotOn(frame,ROOT.RooFit.Components("gtr2Gauss"),ROOT.RooFit.LineStyle(ROOT.kDotted),ROOT.RooFit.LineColor(ROOT.kViolet))
 if not "Krasz" in sys.argv[1]:
   model.plotOn(frame,ROOT.RooFit.Components("n12Gauss"),ROOT.RooFit.LineStyle(ROOT.kDotted),ROOT.RooFit.LineColor(ROOT.kRed))
 if "232" in sys.argv[1]:
@@ -276,6 +279,7 @@ if "208" in sys.argv[1]:
   Z=82.
   N=A-Z
   
+  '''
   #R^2 = unit GT x-section / unit Fermi x-section
   unitGT=109./math.pow(A,0.65) #From fit
   unitF = 72./math.pow(A,1.06) #From fit
@@ -291,6 +295,14 @@ if "208" in sys.argv[1]:
   BF_amp = iasAmp.getVal()
   
   scaleFactor = BF / (BF_amp * Rsquared)
+  '''
+  BF=N-Z
+  BF_amp = iasAmp.getVal()
+  if not "Krasz" in sys.argv[1]:
+    scaleFactor = BF/(1.47*BF_amp) #multiply by ratio GT/F 0-degree cross sections
+  else:
+    scaleFactor=1
+    #TODO
   
   GT1_amp = ROOT.RooRealVar("GT1_amp","GT1_amp",g1Amp.getVal()*scaleFactor)
   GT2_amp = ROOT.RooRealVar("GT2_amp","GT2_amp",g2Amp.getVal()*scaleFactor)
@@ -302,17 +314,15 @@ if "208" in sys.argv[1]:
   GT8_amp = ROOT.RooRealVar("GT8_amp","GT8_amp",gtr1Amp.getVal()*scaleFactor)
   GT9_amp = ROOT.RooRealVar("GT9_amp","GT9_amp",gtr2Amp.getVal()*scaleFactor)
   GT10_amp = ROOT.RooRealVar("GT10_amp","GT10_amp",gtrAmp.getVal()*scaleFactor)
-  #GT11_amp = ROOT.RooRealVar("GT11_amp","GT11_amp",sdrAmp.getVal()*scaleFactor)
-  #GT12_amp = ROOT.RooRealVar("GT12_amp","GT12_amp",qfcAmp.getVal()*scaleFactor)
-  sum=GT1_amp.getVal()+GT2_amp.getVal()+GT3_amp.getVal()+GT4_amp.getVal()+GT5_amp.getVal()+GT6_amp.getVal()+GT7_amp.getVal()+GT8_amp.getVal()+GT9_amp.getVal()+GT10_amp.getVal()#+GT12_amp.getVal()
+  sum=GT1_amp.getVal()+GT2_amp.getVal()+GT3_amp.getVal()+GT4_amp.getVal()+GT5_amp.getVal()+GT6_amp.getVal()+GT7_amp.getVal()+GT8_amp.getVal()+GT9_amp.getVal()+GT10_amp.getVal()
   print("Total GT strength: "+str(sum))
   
   normalizedGaussPdfList=ROOT.RooArgList(g1Gauss,g2Gauss,g3Gauss,g4Gauss,g5Gauss,g6Gauss,g7Gauss)
-  normalizedPdfList=ROOT.RooArgList(gtr1Gauss,gtr2Gauss,gtrGauss)#,qfc)
+  normalizedPdfList=ROOT.RooArgList(gtr1Gauss,gtr2Gauss,gtrGauss)
   normalizedPdfList.add(normalizedGaussPdfList)
   
   normalizedGaussAmpList=ROOT.RooArgList(GT1_amp,GT2_amp,GT3_amp,GT4_amp,GT5_amp,GT6_amp,GT7_amp)
-  normalizedAmpList=ROOT.RooArgList(GT8_amp,GT9_amp,GT10_amp)#,GT12_amp)
+  normalizedAmpList=ROOT.RooArgList(GT8_amp,GT9_amp,GT10_amp)
   normalizedAmpList.add(normalizedGaussAmpList)
   
   normalizedModel=ROOT.RooAddPdf("normalizedModel","normalizedModel",normalizedPdfList,normalizedAmpList)
@@ -323,62 +333,51 @@ if "208" in sys.argv[1]:
   frame2.Draw()
   c1.Modified()
   c1.Update()
+  
+  c3=ROOT.TCanvas("c3","c3")
+  cfdFrame = eVar.frame();
+  cdf = normalizedModel.createCdf(ROOT.RooArgSet(eVar))
+  cdf.plotOn(cfdFrame,ROOT.RooFit.Normalization(sum,ROOT.RooAbsReal.NumEvent));
+  cfdFrame.Draw()
+  c3.Modified()
+  c3.Update()
+  
 else:
   A=232.
   Z=90.
   N=A-Z
-
-  #R^2 = unit GT x-section / unit Fermi x-section
-  unitGT=109./math.pow(A,0.65) #From fit
-  unitF = 72./math.pow(A,1.06) #From fit
-  Rsquared=unitGT/unitF
-
-  #Alternate approach
-  Rsquared = math.pow(E_projectile/55.0,2)
-
-  print("R2 = "+str(Rsquared))
-
+  
   BF=N-Z
-
   BF_amp = iasAmp.getVal()
-
-  scaleFactor = BF / (BF_amp * Rsquared)
-
-  plotBins=600
-
-  '''
-  GT1_amp = ROOT.RooRealVar("GT1_amp","GT1_amp",g1Amp.getVal()*scaleFactor)
-  GT2_amp = ROOT.RooRealVar("GT2_amp","GT2_amp",g2Amp.getVal()*scaleFactor)
-  GT3_amp = ROOT.RooRealVar("GT3_amp","GT3_amp",g3Amp.getVal()*scaleFactor)
-  GT4_amp = ROOT.RooRealVar("GT4_amp","GT4_amp",g4Amp.getVal()*scaleFactor)
-  GT5_amp = ROOT.RooRealVar("GT5_amp","GT5_amp",g5Amp.getVal()*scaleFactor)
-  GT6_amp = ROOT.RooRealVar("GT6_amp","GT6_amp",g6Amp.getVal()*scaleFactor)
-  GT7_amp = ROOT.RooRealVar("GT7_amp","GT7_amp",g7Amp.getVal()*scaleFactor)
-  GT8_amp = ROOT.RooRealVar("GT8_amp","GT8_amp",gtr1Amp.getVal()*scaleFactor)
-  GT9_amp = ROOT.RooRealVar("GT9_amp","GT9_amp",gtr2Amp.getVal()*scaleFactor)
-  GT11_amp = ROOT.RooRealVar("GT11_amp","GT11_amp",sdrAmp.getVal()*scaleFactor)
-  '''
-  GT10_amp = ROOT.RooRealVar("GT10_amp","GT10_amp",gtrAmp.getVal()*scaleFactor)
-  GT12_amp = ROOT.RooRealVar("GT12_amp","GT12_amp",qfcAmp.getVal()*scaleFactor)
-  sum=GT10_amp.getVal()+GT12_amp.getVal()
+  scaleFactor = BF/(1.47*BF_amp) #multiply by ratio GT/F 0-degree cross sections
+  
+  GT1_amp = ROOT.RooRealVar("GT1_amp","GT1_amp",gtr1Amp.getVal()*scaleFactor)
+  #GT2_amp = ROOT.RooRealVar("GT2_amp","GT2_amp",gtr2Amp.getVal()*scaleFactor)
+  GT3_amp = ROOT.RooRealVar("GT3_amp","GT3_amp",gtrAmp.getVal()*scaleFactor)
+  sum=GT1_amp.getVal()+GT3_amp.getVal()
   print("Total GT strength: "+str(sum))
 
-  #normalizedGaussPdfList=ROOT.RooArgList(g1Gauss,g2Gauss,g3Gauss,g4Gauss,g5Gauss,g6Gauss,g7Gauss)
-  normalizedPdfList=ROOT.RooArgList(gtrGauss)
-  #normalizedPdfList.add(normalizedGaussPdfList)
+  normalizedPdfList=ROOT.RooArgList(gtr1Gauss,gtrGauss)
 
-  #normalizedGaussAmpList=ROOT.RooArgList(GT1_amp,GT2_amp,GT3_amp,GT4_amp,GT5_amp,GT6_amp,GT7_amp)
-  normalizedAmpList=ROOT.RooArgList(GT10_amp)
-  #normalizedAmpList.add(normalizedGaussAmpList)
+  normalizedAmpList=ROOT.RooArgList(GT1_amp,GT3_amp)
 
   normalizedModel=ROOT.RooAddPdf("normalizedModel","normalizedModel",normalizedPdfList,normalizedAmpList)
 
-  #c1.cd()
-  #frame2=eVar.frame(30)
-  #normalizedModel.plotOn(frame2,ROOT.RooFit.Normalization(sum,ROOT.RooAbsReal.NumEvent))
-  #frame2.Draw()
-  #c1.Modified()
-  #c1.Update()
+  c1.cd()
+  frame2=eVar.frame(30)
+  normalizedModel.plotOn(frame2,ROOT.RooFit.Normalization(sum,ROOT.RooAbsReal.NumEvent))
+  frame2.Draw()
+  c1.Modified()
+  c1.Update()
+  
+  c3=ROOT.TCanvas("c3","c3")
+  cfdFrame = eVar.frame();
+  cdf = normalizedModel.createCdf(ROOT.RooArgSet(eVar))
+  cdf.plotOn(cfdFrame,ROOT.RooFit.Normalization(sum,ROOT.RooAbsReal.NumEvent));
+  cfdFrame.Draw()
+  c3.Modified()
+  c3.Update()
+  
 try:
   input("Press enter to continue")
 except SyntaxError:
